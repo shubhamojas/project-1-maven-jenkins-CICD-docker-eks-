bff4192b253257aad930884e5dd416e1014f8a02
shubhamojas <shubhamsri411@yahoo.com>
2024-08-06 17:27:31 +0530

    Update backend.tf

--- eks-terraform/backend.tf
+++ eks-terraform/backend.tf
@@ -9,7 +9,7 @@ terraform {
   backend "s3" {
     bucket = "shubhamprojectnew"
     key    = "eks/terraform.tfstate"
-    region = "us-east-1"
+    region = "ap-south-1"
   }
 
   required_version = ">= 1.6.3"
