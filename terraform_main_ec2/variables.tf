5723f7199ea707c2823bbf95d46f99f57877f11a
shubhamojas <shubhamsri411@yahoo.com>
2024-08-06 17:24:58 +0530

    Update variables.tf

--- terraform_main_ec2/variables.tf
+++ terraform_main_ec2/variables.tf
@@ -1,7 +1,7 @@
 variable "region" {
   description = "AWS region"
   type = string
-  default = "us-east-1"
+  default = "ap-south-1"
 }
 
 variable "vpc-name" {
@@ -56,7 +56,7 @@ variable "instance_type" {
 variable "key_name" {
   description = "EC2 keypair"
   type        = string
-  default     = "gitopskey"
+  default     = "dockerdevops"
 }
 
 variable "instance_name" {
