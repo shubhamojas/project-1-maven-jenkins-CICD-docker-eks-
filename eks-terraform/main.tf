17bdc21b954ffc85b885f268a2482ad21ccb381a
shubhamojas <shubhamsri411@yahoo.com>
2024-08-09 17:32:39 +0530

    Update main.tf

--- eks-terraform/main.tf
+++ eks-terraform/main.tf
@@ -110,86 +110,97 @@ provider "aws" {
     role       = aws_iam_role.worker.name
   }
  
- # data source 
- data "aws_vpc" "main" {
-  tags = {
-    Name = "Jumphost-vpc"  # Specify the name of your existing VPC
+ # Data source for VPC
+data "aws_vpc" "main" {
+  filter {
+    name   = "tag:Name"
+    values = ["Jumphost-vpc"]
+  }
+
+  # Optionally add more specific filters if necessary
+  filter {
+    name   = "cidr"
+    values = ["10.0.0.0/16"]
   }
 }
 
-data "aws_subnet" "subnet_1" {
- vpc_id = data.aws_vpc.main.id
- filter {
-    name = "tag:Name"
-    values = ["Jumphost-subnet1"]
- }
+# Data sources for subnets
+data "aws_subnet" "subnet-1" {
+  vpc_id = data.aws_vpc.main.id
+  filter {
+    name   = "tag:Name"
+    values = ["MyPublicSubnet01"]
+  }
 }
 
-data "aws_subnet" "subnet_2" {
- vpc_id = data.aws_vpc.main.id
- filter {
-    name = "tag:Name"
-    values = ["Jumphost-subnet2"]
- }
+data "aws_subnet" "subnet-2" {
+  vpc_id = data.aws_vpc.main.id
+  filter {
+    name   = "tag:Name"
+    values = ["MyPublicSubnet02"]
+  }
 }
+
+# Data source for security group
 data "aws_security_group" "selected" {
   vpc_id = data.aws_vpc.main.id
   filter {
-    name = "tag:Name"
-    values = ["Jumphost-sg"]
- }
+    name   = "tag:Name"
+    values = ["devops-project-veera"]
+  }
 }
 
- #Creating EKS Cluster
-  resource "aws_eks_cluster" "eks" {
-    name     = "project-eks"
-    role_arn = aws_iam_role.master.arn
+# EKS Cluster and Node Group resources
+resource "aws_eks_cluster" "eks" {
+  name     = "project-eks"
+  role_arn = aws_iam_role.master.arn
 
-    vpc_config {
-      subnet_ids = [data.aws_subnet.subnet_1.id, data.aws_subnet.subnet_2.id]
-    }
-
-    tags = {
-      "Name" = "MyEKS"
-    }
-
-    depends_on = [
-      aws_iam_role_policy_attachment.AmazonEKSClusterPolicy,
-      aws_iam_role_policy_attachment.AmazonEKSServicePolicy,
-      aws_iam_role_policy_attachment.AmazonEKSVPCResourceController,
-    ]
+  vpc_config {
+    subnet_ids = [data.aws_subnet.subnet-1.id, data.aws_subnet.subnet-2.id]
   }
- resource "aws_eks_node_group" "node-grp" {
-    cluster_name    = aws_eks_cluster.eks.name
-    node_group_name = "project-node-group"
-    node_role_arn   = aws_iam_role.worker.arn
-    subnet_ids      = [data.aws_subnet.subnet_1.id, data.aws_subnet.subnet_2.id]
-    capacity_type   = "ON_DEMAND"
-    disk_size       = 20
-    instance_types  = ["t2.small"]
 
-    remote_access {
-      ec2_ssh_key               = "dockerdevops"
-      source_security_group_ids = [data.aws_security_group.selected.id]
-    }
-
-    labels = {
-      env = "dev"
-    }
-
-    scaling_config {
-      desired_size = 2
-      max_size     = 4
-      min_size     = 1
-    }
-
-    update_config {
-      max_unavailable = 1
-    }
-
-    depends_on = [
-      aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
-      aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
-      aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
-    ]
+  tags = {
+    "Name" = "MyEKS"
   }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.AmazonEKSServicePolicy,
+    aws_iam_role_policy_attachment.AmazonEKSVPCResourceController,
+  ]
+}
+
+resource "aws_eks_node_group" "node-grp" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "project-node-group"
+  node_role_arn   = aws_iam_role.worker.arn
+  subnet_ids      = [data.aws_subnet.subnet-1.id, data.aws_subnet.subnet-2.id]
+  capacity_type   = "ON_DEMAND"
+  disk_size       = 20
+  instance_types  = ["t2.small"]
+
+  remote_access {
+    ec2_ssh_key               = "provisioner"
+    source_security_group_ids = [data.aws_security_group.selected.id]
+  }
+
+  labels = {
+    env = "dev"
+  }
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 4
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
